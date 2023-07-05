ab91e9e67553d497036ca7c0e78940565c2db9ea
Benjamin Davis <allrisc.dev@gmail.com>
2023-07-05 16:31:54 -0700

    Created compiled stub files and compile bld file

---
+++ src/uart_if.sv
@@ -0,0 +1,26 @@
+/************************************************************************************
+ * Repo to demonstrate layered UVM sequences
+ * Copyright (C) 2023  RISCY-Lib Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ************************************************************************************/
+
+interface uart_if;
+  logic rx;
+  logic tx;
+
+  `include "uvm_macros.svh"
+  import uvm_pkg::*;
+
+endinterface
